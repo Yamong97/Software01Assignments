1bbc53898bf8b4990e17cb64fe3aa88d0ad55e1f
Yamong <yamongphoo012@gmail.com>
2025-08-29 20:11:05 +0630

    Please let me commit and push Module4_Task06 again.

--- Module04/Task06.py
+++ Module04/Task06.py
@@ -1,19 +1,20 @@
 import random
 
-N = int(input("Please enter the total number of total random points: "))
+N = int(input("Please enter total random points: "))
 
 points_in_circle = 0
 total_points = 0
 
 while total_points < N:
-    a = random.uniform(-1,1)
-    b = random.uniform(-1,1)
+    x = random.uniform(-1,1)
+    y = random.uniform(-1,1)
 
-    if a**2 + b**2 <1:
+    if x**2 + y**2 <1:
         points_in_circle += 1
 
-        total_points += 1
+    total_points += 1
 
 pi = 4*points_in_circle/N
 
-print(f"The approximation of the value of pi is {pi}")
+print(f"There are {points_in_circle} points in the circle.")
+print(f"The approximation of the value of pi is {pi}.")
